7abcc64b1502774dd9988b4c5db7c5ab54771041
yuanlin <yuanlin2008@hotmail.com>
2018-03-21 23:23:36 +0800

    123

--- Source/Lua/Lua.Build.cs
+++ Source/Lua/Lua.Build.cs
@@ -7,6 +7,7 @@ public class Lua : ModuleRules
 	public Lua(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+		PublicDependencyModuleNames.AddRange(new string[] { "Core" });
         Definitions.Add("LUA_PLATFORM_" + Target.Platform.ToString());
         //if(Target.LinkType == TargetLinkType.Monolithic)
         //    Definitions.Add("LUA_BUILD_AS_DLL");
